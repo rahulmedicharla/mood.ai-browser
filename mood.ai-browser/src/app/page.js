a2fdfc2c53ada36d5d72991e2774605ee3cd6f14
medicharla2 <98125181+medicharla2@users.noreply.github.com>
2023-05-18 15:01:40 -0400

    put firebase config to env file

--- mood.ai-browser/src/app/page.js
+++ mood.ai-browser/src/app/page.js
@@ -2,7 +2,6 @@
 //firebase imports
 import {initializeApp } from 'firebase/app'
 import { initializeAuth, browserSessionPersistence } from 'firebase/auth'
-import { firebaseConfig } from '../../firebaseConfig'
 
 //react-redux imports
 import { store } from "../redux/store"
@@ -10,6 +9,15 @@ import { Provider } from 'react-redux'
 
 //view imports 
 import App from './navigation/page'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig)
 initializeAuth(app, {
